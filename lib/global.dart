c2dc513bff938dc303fca3665a2becaae634a908
nirmalk <nirmalkarthikeyan1503@gmail.com>
2021-10-23 15:52:54 +0530

    Completed Upload.dart

---
+++ lib/global.dart
@@ -0,0 +1,4 @@
+import 'dart:ui';
+
+const textColor = Color(0xFFF700FF);
+const buttonColor = Color(0xff3D2C8D);
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:emergency_services/Home.dart';
-import 'package:emergency_services/Service.dart';
-import 'package:emergency_services/ServiceBody.dart';
+import 'package:emergency_services/Upload.dart';
 import 'package:flutter/material.dart';
-import 'ServiceBody.dart';
 
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: SafeArea(
-      child: Service(),
+      child: ReportCrime(),
     ),
   ));
 }
