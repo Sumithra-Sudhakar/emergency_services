b04f3a4d3cc5b91bac512fff5906f1871194c3ee
Maitthreyan11 <maithreyan11@gmail.com>
2021-10-23 16:03:31 +0530

    Login and SignUp

--- lib/Login.dart
+++ lib/Login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'SignUp.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -8,10 +10,179 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("login"),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0xffF6E6CB),
+        resizeToAvoidBottomInset: false,
+        body: Stack(
+          children: <Widget>[
+            Container(
+              child: SafeArea(
+                child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: ListView(
+                      children: <Widget>[
+                        SizedBox(
+                          height: 150,
+                        ),
+                        Container(
+                            alignment: Alignment.center,
+                            padding: EdgeInsets.all(10),
+                            child: Text(
+                              'Log in',
+                              style: GoogleFonts.gentiumBasic(
+                                fontStyle: FontStyle.normal,
+                                fontSize: 50,
+                                fontWeight: FontWeight.w600,
+                                color: Color(0xff6C4A4A),
+                              ),
+                            )),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: TextField(
+                            cursorColor: Color(0xffEC524B),
+                            controller: nameController,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(20),
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)),
+                                borderSide: const BorderSide(
+                                    color: Color(0xffEC524B), width: 2.0),
+                              ),
+                              labelText: 'Username',
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                          child: TextField(
+                            cursorColor: Color(0xffEC524B),
+                            obscureText: true,
+                            controller: passwordController,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)),
+                                borderSide: const BorderSide(
+                                    color: Color(0xffEC524B), width: 2.0),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)),
+                                borderSide: const BorderSide(
+                                    color: Color(0xffEC524B), width: 2.0),
+                              ),
+                              labelText: 'Password',
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Center(
+                          child: ElevatedButton.icon(
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(25.0)),
+                              primary: Color(0xffA0937D),
+                              padding: EdgeInsets.all(15),
+                              onPrimary: Color(0xffF3EAC2),
+                              elevation: 5,
+                            ),
+                            label: Text('Log In'),
+                            onPressed: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => Login()));
+                            },
+                            icon: Icon(Icons.assignment_turned_in),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        FlatButton(
+                          onPressed: () {
+                            //forgot password screen
+                          },
+                          textColor: Color(0xff6C4A4A),
+                          child: Text(
+                            'Forgot Password',
+                            style: GoogleFonts.gentiumBasic(
+                              fontStyle: FontStyle.normal,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xff6C4A4A),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          height: 30,
+                        ),
+                        Container(
+                            child: Row(
+                          children: <Widget>[
+                            Text(
+                              'Do not have an account?',
+                              style: GoogleFonts.gentiumBasic(
+                                fontStyle: FontStyle.normal,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w400,
+                                color: Color(0xff6C4A4A),
+                              ),
+                            ),
+                            FlatButton(
+                              textColor: Colors.blue,
+                              child: Container(
+                                child: Row(
+                                  children: [
+                                    Text('Sign Up',
+                                        style: GoogleFonts.gentiumBasic(
+                                          fontStyle: FontStyle.normal,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w400,
+                                          color: Color(0xffC68B59),
+                                        )),
+                                    Container(
+                                      width: 10,
+                                    ),
+                                    Icon(Icons.arrow_drop_down_circle_outlined,
+                                        color: Color(0xffC68B59))
+                                  ],
+                                ),
+                              ),
+                              onPressed: () {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => SignUp()));
+                                //signup screen
+                              },
+                            )
+                          ],
+                          mainAxisAlignment: MainAxisAlignment.center,
+                        ))
+                      ],
+                    )),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/SignUp.dart
+++ lib/SignUp.dart
@@ -1,17 +1,241 @@
 import 'package:flutter/material.dart';
+import 'Login.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SignUp extends StatefulWidget {
-  const SignUp({Key? key}) : super(key: key);
-
   @override
-  _SignUpState createState() => _SignUpState();
+  _State createState() => _State();
 }
 
-class _SignUpState extends State<SignUp> {
+class VerticalText extends StatefulWidget {
+  @override
+  _VerticalTextState createState() => _VerticalTextState();
+}
+
+class _VerticalTextState extends State<VerticalText> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("signup"),
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, right: 330),
+    );
+  }
+}
+
+class _State extends State<SignUp> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController locationController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0xffF6E6CB),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Sign Up',
+                      style: GoogleFonts.gentiumBasic(
+                        fontStyle: FontStyle.normal,
+                        fontSize: 50,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xff6C4A4A),
+                      ),
+                    )),
+                Center(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    child: TextField(
+                      controller: usernameController,
+                      decoration: const InputDecoration(
+                        labelText: 'Username',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20),
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          borderSide: const BorderSide(
+                              color: Color(0xffEC524B), width: 2.0),
+                        ),
+                        hintText: 'Enter a nice, unique username!',
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Center(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    child: TextField(
+                      controller: emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                        labelText: 'E-Mail',
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20))),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          borderSide: const BorderSide(
+                              color: Color(0xffEC524B), width: 2.0),
+                        ),
+                        hintText: 'E-mail?',
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Center(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    child: TextField(
+                      controller: passwordController,
+                      keyboardType: TextInputType.visiblePassword,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20))),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          borderSide: const BorderSide(
+                              color: Color(0xffEC524B), width: 2.0),
+                        ),
+                        hintText: 'Secret, super safe password here.',
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Center(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    child: TextField(
+                      controller: locationController,
+                      decoration: const InputDecoration(
+                        labelText: 'Location',
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20))),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          borderSide: const BorderSide(
+                              color: Color(0xffEC524B), width: 2.0),
+                        ),
+                        hintText: 'Where do you live?',
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Center(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    child: TextField(
+                      controller: phoneController,
+                      keyboardType: TextInputType.phone,
+                      decoration: const InputDecoration(
+                        labelText: 'Phone',
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20))),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          borderSide: const BorderSide(
+                              color: Color(0xffEC524B), width: 2.0),
+                        ),
+                        hintText: 'Phone number :)',
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Center(
+                  child: ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25.0)),
+                      primary: Color(0xffA0937D),
+                      padding: EdgeInsets.all(15),
+                      onPrimary: Color(0xffF3EAC2),
+                      elevation: 5,
+                    ),
+                    label: Text('Sign Up'),
+                    onPressed: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => SignUp()));
+                    },
+                    icon: Icon(Icons.assignment_turned_in),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Have an account already?',
+                        style: GoogleFonts.gentiumBasic(
+                          fontStyle: FontStyle.normal,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xff6C4A4A),
+                        ),
+                      ),
+                      FlatButton(
+                        child: Container(
+                          child: Text(
+                            'Sign in',
+                            style: GoogleFonts.gentiumBasic(
+                              fontStyle: FontStyle.normal,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xffC68B59),
+                            ),
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).push(
+                              MaterialPageRoute(builder: (context) => Login()));
+                          //signup screen
+                        },
+                      )
+                    ],
+                  ),
+                ),
+                VerticalText(),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
