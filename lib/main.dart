d8b091b20dad9958ace2012007863fe839e316f9
Sumithra-Sudhakar <sumisooriya@gmail.com>
2021-10-23 15:51:14 +0530

    Service part done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:emergency_services/Home.dart';
+import 'package:emergency_services/Service.dart';
+import 'package:emergency_services/ServiceBody.dart';
 import 'package:flutter/material.dart';
+import 'ServiceBody.dart';
 
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: SafeArea(
-      child: Home(),
+      child: Service(),
     ),
   ));
 }
