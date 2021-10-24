77e04c59d9c9d14269a0f7b46f7522e110e9c832
nirmalk <nirmalkarthikeyan1503@gmail.com>
2021-10-24 17:06:08 +0530

    Auth works

--- lib/Login.dart
+++ lib/Login.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'SignUp.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -13,6 +14,15 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+
+  Future<UserCredential> signIn() async {
+    UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: nameController.text,
+        password: passwordController.text
+    );
+
+    return await FirebaseAuth.instance.signInWithEmailAndPassword(email: nameController.text,password: passwordController.text);
+  }
 
   @override
   Widget build(BuildContext context) {
